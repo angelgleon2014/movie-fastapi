caba61b9c25b108d764852cab90ac513fb648688
Angel Leon <angelgleon@hotmail.com>
2025-06-21 19:41:05 -0400

    Despliegue en server

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.responses import HTMLResponse
 from bd.database import engine, Base
 from routers.movie import routerMovie
 from routers.users import routerAuth
+import uvicorn
+import os
 
 app = FastAPI(
     title="My FastAPI Application",
@@ -18,3 +20,9 @@ Base.metadata.create_all(bind=engine) # crea las tablas en la base de datos si n
 @app.get("/", tags=["Inicio"])
 def read_root():
     return HTMLResponse('<h2>Welcome to My FastAPI Application</h2><p>This is a sample application to demonstrate FastAPI features.</p>')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Obtiene el puerto del entorno o usa 8000 por defecto
+    uvicorn.run(app, host="0.0.0.0", port=port)
+    # uvicorn.run("main:app", host="0.0.0.0", port=port)
